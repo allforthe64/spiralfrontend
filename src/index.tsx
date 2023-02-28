0c1505bdfb33e066f4049768085fb064cf4207c9
William Rainey <williamhrainey@gmail.com>
2023-02-28 09:06:51 -0800

    added disable dev tools package

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
