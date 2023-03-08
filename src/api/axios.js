2b7c81af8fe6bbdcd431b620570da83809b03559
William Rainey <williamhrainey@gmail.com>
2023-03-08 11:26:13 -0800

    changed axios base url to match backend api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:3500'
+const BASE_URL = 'https://spiral-backend-api.onrender.com'
 
 export default axios.create({
     baseURL: BASE_URL
