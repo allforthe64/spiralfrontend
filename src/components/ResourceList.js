9e28007878692cc8aa2d1075a19f16a6c1302ed9
William Rainey <williamhrainey@gmail.com>
2023-03-21 16:13:43 -0700

    finished filter functionality

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
-import { useNavigate, useLocation, Link } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 
 
