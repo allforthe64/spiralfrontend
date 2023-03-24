97de940243e182d3e8a8a64a5d7f77d010d5642c
William Rainey <williamhrainey@gmail.com>
2023-03-24 14:41:08 -0700

    completed basic styling of the dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,7 +7,12 @@ const Dashboard = () => {
     return (
         <div className="py-10 headings text-white pr-6">
             <h2 className="font-bold text-5xl mb-20">My Dashboard</h2>
-            <MGoals />
+            <div className="flex justify-around pl-[1%] mb-16">
+                <div className="h-[350px] w-[31%]  border border-white">Profile Pic goes here</div>
+                <div className="w-[63%] ml-[2%]">
+                    <MGoals />
+                </div>
+            </div>
             <div className="flex justify-around">
                 <div className="w-9/12">
                     <Goals />
--- src/components/MGoals.js
+++ src/components/MGoals.js
@@ -40,24 +40,34 @@ const MGoals = () => {
 
     return (
         <article>
-            <h2>Monthly Goals List</h2>
-            {mgoals?.length
-                ? (
-                    <ul>
-                        {mgoals.map((mgoal) => (
-                        <li key={mgoal?._id} className="text-white">
-                            {mgoal?.title}
-                            <Link to={`/mgoals/${mgoal._id}`} className="text-red-700">Edit</Link>
-                        </li>
-                        ))}
-                    </ul>
-                ) : (
-                    <div>
-                        <p>No monthly goals to display</p>
+            <div className="border-2 p-4 bg-slate-900 border-gray-500 rounded-lg">
+                <div className="flex justify-center mb-6 border-b-2 border-white">
+                    <div className="flex justify-around w-9/12">
+                        <h2 className="text-2xl headings font-bold">Monthly Goals List</h2>
+                        <button className="info-txt font-bold mb-12 bg-alien-green py-px px-8 rounded-md text-black"><Link to={'/mgoals/new'}>Add New Goal</Link></button>
                     </div>
-                )
-            }
-            <Link to={'/mgoals/new'} className="text-yellow-500">Add New Monthly Goal</Link>
+                </div>
+                <div className="h-[219px] pt-4 overflow-auto scrollbar">
+                    {mgoals?.length ? (
+                        <div className="h-[200px] w-8/12 pt-4 pl-8 overflow-auto scrollbar">
+                            <ul>
+                                {mgoals.map((mgoal) => (
+                                    <div className="flex justify-between mb-4">
+                                        <li key={mgoal?._id} className="text-white">
+                                            <p className="ml-2 font-bold text-lg leading-tight headings">{mgoal?.title}</p>
+                                        </li>
+                                        <button className="text-red-700 mr-20 leading-relaxed info-txt font-bold hover:underline"><Link to={`/mgoals/${mgoal._id}`}>Edit</Link></button>
+                                    </div>
+                                ))}
+                            </ul>
+                        </div>
+                        ) : (
+                            <div>
+                                <p>No monthly goals to display</p>
+                            </div>
+                        )}
+                </div>
+            </div>
         </article>
     );
 };
