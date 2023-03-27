ab946bfbed41799c42eadaefe9bbd516f71086c7
William Rainey <williamhrainey@gmail.com>
2023-03-27 16:22:16 -0700

    refactored new goal/mgoal/note components to include NewForm component

--- src/components/NewNoteForm.js
+++ src/components/NewNoteForm.js
@@ -2,6 +2,7 @@ import { useState} from "react";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
 import { useNavigate, Link } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
+import NewForm from "./NewForm";
 
 
 const NewNoteForm = () => {
@@ -59,41 +60,7 @@ const NewNoteForm = () => {
 
     return (
         <>
-            <div className="py-20">
-                <h4 className="text-white headings text-5xl font-bold mb-6">Add a note for {auth.user}</h4>
-                <p className="text-white info-txt text-2xl">Leave yourself a note about what you accomplished today!</p>
-            </div>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <input
-                    type="text"
-                    placeholder="Enter a note title"
-                    className="w-4/12 bg-inherit border-b-2 outline-0 text-white info-txt text-2xl pl-2 pb-px mb-20"
-                    onChange={(e) => handleTitle(e)}
-                    value={title}
-                    />
-                </div>
-
-                <div>
-                    <textarea
-                    type="text"
-                    placeholder="Enter note body"
-                    className="w-4/12 bg-inherit border-2 outline-0 text-white info-txt text-lg p-2 mb-24 rounded-md"
-                    onChange={(e) => handleText(e)}
-                    value={text}
-                    rows='8'
-                    />
-                </div>
-
-                <div>
-                    <button type="submit" className="info-txt font-bold text-2xl mb-8 bg-alien-green py-px px-8 rounded-md">
-                    Submit
-                    </button>
-                </div>
-            </form>
-            <div className="py-10">
-                <Link to={'/dashboard'} className='text-white hover:underline'>Return to dashboard</Link>
-            </div>
+            <NewForm note={true} user={auth.user} submit={handleSubmit} handleTitle={handleTitle} handleText={handleText} title={title} text={text}/>
         </>
     );
 };
