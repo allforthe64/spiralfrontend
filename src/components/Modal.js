007d8ab632bc290e4fd22029aa206d97b1988994
William Rainey <williamhrainey@gmail.com>
2023-04-05 15:23:38 -0700

    styled modal.js for mobile

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -33,7 +33,7 @@ const Modal = (props) => {
     let tutArr = []
 
     for (let i in props.tutorials) {
-        let element = <li className="text-white text-left"><Link to={props.tutorials[i].link} target='_blank' className="text-white info-txt mt-4 tutorial text-lg">{props.tutorials[i].title}</Link></li>
+        let element = <li className="text-white text-left"><Link to={props.tutorials[i].link} target='_blank' className="text-white info-txt mt-4 tutorial text-lg sm:max-md:text-sm max-sm:text-sm">{props.tutorials[i].title}</Link></li>
         tutArr.push(element)
     }
 
@@ -69,29 +69,29 @@ const Modal = (props) => {
     }
 
     return (
-        <div className="modal backdrop-blur-sm flex justify-center py-20">
-            <div className="border w-10/12 bg-dark rounded-2xl pb-26 h-[100%]">
-                <div className="flex justify-end pt-2 pr-4">
+        <div className="modal backdrop-blur-sm flex justify-center py-16">
+            <div className="border w-10/12 bg-dark rounded-2xl">
+                <div className="flex justify-end pt-2 pr-4 max-sm:mb-3">
                     <button className="text-white" onClick={() => props.onClickFunc()}><FontAwesomeIcon icon={faXmark} className='text-4xl' /></button>
                 </div>
-                <h1 className="text-white mb-8 info-txt font-bold text-6xl">{props.name}</h1>
-                <div className="overflow-auto h-[90%] w-[99%] py-8 modal-scroll">
-                    <div className="h-[70%]">
-                        <div className="ml-10 mb-4">
-                            <p className="text-white text-left info-txt">{stringArr[0]}</p>
+                <h1 className="text-white mb-4 info-txt font-bold md:text-6xl sm:max-md:text-4xl max-sm:text-2xl max-sm:mb-px ">{props.name}</h1>
+                <div className="h-[90%] w-[99%] py-8 ">
+                    <div className="h-[70%] flex flex-col items-left pl-[5%] overflow-auto mb-4 max-sm:mb-6 modal-scroll">
+                        <div className="w-10/12 mb-4">
+                            <p className="text-white text-left info-txt sm:max-md:text-sm max-sm:text-xs">{stringArr[0]}</p>
                         </div>
-                        <p className="text-white text-left info-txt font-bold text-xl ml-10 mb-10">{'Cost' + stringArr[1]}</p>
-                        <div className="pl-10">
-                            <h2 className="info-txt font-bold text-2xl text-white text-left">Tutorials:</h2>
-                            <ul className="list-disc pl-8 pt-2 mb-8">
+                        <p className="text-white text-left info-txt font-bold text-xl md:mb-4 max-sm:mb-2 sm:max-md:text-base sm:max-md:mb-4 max-sm:text-base">{'Cost' + stringArr[1]}</p>
+                        <div className="pl-10 max-sm:pl-3">
+                            <h2 className="info-txt font-bold text-2xl text-white text-left sm:max-md:text-lg max-sm:text-base">Tutorials:</h2>
+                            <ul className="list-disc pl-8 pt-2">
                                 {tutArr}
                             </ul>
                         </div>
                     </div>
-                    <div className='flex justify-center'>
-                        <div className='w-5/12 flex justify-around'>
-                            <button className="bg-alien-green info-txt px-4 py-2 text-xl font-bold rounded-lg"><Link to={props.link} target='_blank'>Start using {props.name}</Link></button>
-                            <FontAwesomeIcon onClick={toggleFav} icon={[`${favorited}`, "heart"]} className='text-white text-5xl hover:cursor-pointer'/>
+                    <div className='flex justify-center sm:max-md:mt-4'>
+                        <div className='md:w-10/12 sm:max-md:w-8/12 flex justify-around'>
+                            <button className="bg-alien-green info-txt px-4 py-2 text-xl font-bold rounded-lg sm:max-md:text-sm max-sm:text-xs max-sm:w-8/12"><Link to={props.link} target='_blank'>Start using {props.name}</Link></button>
+                            <FontAwesomeIcon onClick={toggleFav} icon={[`${favorited}`, "heart"]} className='text-white text-5xl max-sm:text-3xl max-sm:mt-2 sm:max-md:text-3xl sm:max-md:mt-px hover:cursor-pointer'/>
                         </div>
                     </div>
                     
